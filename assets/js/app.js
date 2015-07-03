b9e2059c454bb49e20f6874ae9b8fb6eb7119a28
Miguel <galacemiguel@gmail.com>
2015-07-03 17:22:28 +0800

    updated sched

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 	var hr = currentDate.getHours();
 	var min = currentDate.getMinutes();
 	var sec = currentDate.getSeconds();
+
 	window.setInterval(function() {
 		currentDate = new Date();
 		day = currentDate.getDay();
@@ -24,12 +25,13 @@ $(document).ready(function() {
 			$('.semi-circle2').css('z-index', '1');
 		}
 	},1000);
+
 	/*
 		// Date Simulator
-		day = 2;
-		hr = 14;
-		min = 0;
-		sec = 0;
+		day = 3;
+		hr = 13;
+		min = 19;
+		sec = 50;
 		window.setInterval(function() {
 			if (sec < 59) {
 				sec++;
@@ -71,16 +73,16 @@ $(document).ready(function() {
 		},1000);
 	*/
 
-	var Period = function(title, venue, time, color) {
+	var Period = function(title, room, sched, color) {
 		this.title = title;
-		this.venue = venue;
-		this.time = time;
+		this.room = room;
+		this.sched = sched;
 		this.color = color;
 	}
 
 	Period.prototype.getLength = function() {
-		var periodStart = this.time.split("-")[0];
-		var periodEnd = this.time.split("-")[1];
+		var periodStart = this.sched.split("-")[0];
+		var periodEnd = this.sched.split("-")[1];
 
 		var hrStart = parseInt(periodStart.split(":")[0]);
 		var minStart = parseInt(periodStart.split(":")[1]);
@@ -90,19 +92,14 @@ $(document).ready(function() {
 		return 60*(60*(hrEnd - hrStart) - minStart + minEnd);
 	}
 
-	var cs = new Period("CS 21B", "F-227", "8:30-10:20", "#BFBFBF");
-	var en = new Period("EN 101", "SEC-A 205A", "9:30-10:20", "#2ECC71");
-	var fil = new Period("FIL 12", "K-203", "13:30-14:20", "#F2784B");
-	var intac = new Period("INTAC 2", "CTC-107", "14:30-15:20", "#22A7F0");
-	var lit = new Period("LIT 14", "SEC-A 205A", "10:30-11:20", "#BF55EC");
-	var ma = new Period("MA 20.2", "", "", "#22313F");
-	var pe = new Period("PE 112", "COV COURTS", "14:00-14:50", "#F9BF3B");
+	var flc = new Period("FLC 1SP", "G-304", "9:00-10:20", "#F9BF3B");
+	var psy = new Period("PSY 101", "B-106", "12:00-13:20", "#19B5FE");
 	var breakPeriod = new Period("", "", "", "#C0392B");
 
 	var secsLeft;
-	function computeSecsLeft(periodTime) {
-		var periodStart = periodTime.split("-")[0];
-		var periodEnd = periodTime.split("-")[1];
+	function computeSecsLeft(periodSched) {
+		var periodStart = periodSched.split("-")[0];
+		var periodEnd = periodSched.split("-")[1];
 
 		var hrEnd = parseInt(periodEnd.split(":")[0]);
 		var minEnd = parseInt(periodEnd.split(":")[1]);
@@ -112,7 +109,7 @@ $(document).ready(function() {
 
 	var secsElapsed;
 	var deg;
-	function convertToDeg(secsLeft, length) {
+	function convertToDegrees(secsLeft, length) {
 		secsElapsed = length - secsLeft;
 		deg = secsElapsed / length * 360;
 	}
@@ -121,8 +118,8 @@ $(document).ready(function() {
 	function updateDisplay(period) {
 		currentPeriod = period;
 
-		computeSecsLeft(period.time);
-		convertToDeg(secsLeft, period.getLength());
+		computeSecsLeft(period.sched);
+		convertToDegrees(secsLeft, period.getLength());
 		$('.period').html(period.title);
 		window.setTimeout(function() {
 			$('.clock-wipe-mask').css('border', '2px solid ' + period.color);
@@ -131,167 +128,48 @@ $(document).ready(function() {
 	}
 
 	function updatePeriod() {
-		// Monday, Wednesday, Friday
-		if (day == 1 || day == 3 || day == 5) {
-			// 12:00-7:29AM
-			if (hr < 7 || (hr == 7 && min < 30)) {
-				breakPeriod.title = "PREP";
-				breakPeriod.time = "0:00-7:30";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("MA 20.2/SEC-A 210A");
-			}
-			// 7:30-8:19AM
-			else if ((hr == 7 && min >= 30) || (hr == 8 && min < 20)) {
-				ma.venue = "SEC-A 210A";
-				ma.time = "7:30-8:20";
-				updateDisplay(ma);
-				$('.next-period').html("60MIN BREAK");
-			}
-			// 8:20-9:19AM
-			else if ((hr == 8 && min >= 20) || (hr == 9 && min < 20)) {
-				breakPeriod.title = "60MIN BREAK";
-				breakPeriod.time = "8:20-9:20";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("EN 101/SEC-A 205A");
-			}
-			// 9:20-9:29AM
-			else if (hr == 9 && (min >= 20 && min < 30)) {
-				breakPeriod.title = "10MIN BREAK";
-				breakPeriod.time = "9:20-9:30";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("EN 101/SEC-A 205A");
-			}
-			// 9:30-10:19AM
-			else if ((hr == 9 && min >= 30) || (hr == 10 && min < 20)) {
-				updateDisplay(en);
-				$('.next-period').html("LIT 14/SEC-A 205A");
-			}
-			// 10:20-10:29AM
-			else if (hr == 10 && (min >= 20 && min < 30)) {
-				breakPeriod.title = "10MIN BREAK";
-				breakPeriod.time = "10:20-10:30";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("LIT 14/SEC-A 205A");
-			}
-			// 10:30-11:19AM
-			else if ((hr == 10 && min >= 30) || (hr == 11 && min < 20)) {
-				updateDisplay(lit);
-				$('.next-period').html("120MIN BREAK");
-			}
-			// 11:20-1:19PM
-			else if ((hr == 11 && min >= 20) || hr == 12 || (hr == 13 && min < 20)) {
-				breakPeriod.title = "120MIN BREAK";
-				breakPeriod.time = "11:20-13:20";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("FIL 12/K-203");
-			}
-			// 1:20-1:29PM
-			else if (hr == 13 && (min >= 20 && min < 30)) {
-				breakPeriod.title = "10MIN BREAK";
-				breakPeriod.time = "13:20-13:30";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("FIL 12/K-203");
-			}
-			// 1:30-2:19PM
-			else if ((hr == 13 && min >= 30) || (hr == 14 && min < 20)) {
-				updateDisplay(fil);
-				if (day == 5) {
-					$('.next-period').html("INTAC 2/CTC-107");
-				} else {
-					window.setTimeout(function() {
-						$('.container').height('424px');
-						$('footer').hide();
-					},4);
-				}
-			}
-			// Friday, 2:20-2:29PM
-			else if (day == 5 && hr == 14 && (min >= 20 && min < 30)) {
-				breakPeriod.title = "10MIN BREAK";
-				breakPeriod.time = "14:20-14:30";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("INTAC 2/CTC-107");
-			}
-			// Friday, 2:30-3:19PM
-			else if (day == 5 && ((hr == 14 && min >= 30) || (hr == 15 && min < 20))) {
-				updateDisplay(intac);
-				window.setTimeout(function() {
-					$('.container').height('424px');
-					$('footer').hide();
-				},4);
-			} else {
-				$('.period').html("FIN");
+		// 12:00-8:59AM
+		if (hr < 9) {
+			breakPeriod.title = "PREP";
+			breakPeriod.sched = "0:00-9:00";
+			updateDisplay(breakPeriod);
+			$('.next-period').html("FLC 1SP/G-304");
+		}
+		// 9:00-10:19AM
+		else if (hr == 9 || (hr == 10 && min < 20)) {
+			updateDisplay(flc);
+			$('.next-period').html("90MIN BREAK");
+		}
+		// 10:20-11:49AM
+		else if ((hr == 10 && min >= 20) || (hr == 11 && min < 50)) {
+			breakPeriod.title = "90MIN BREAK";
+			breakPeriod.sched = "10:20-11:50";
+			updateDisplay(breakPeriod);
+			$('.next-period').html("PSY 101/B-106");
+		}
+		// 11:50-11:59AM
+		else if (hr == 11 && min >= 50) {
+			breakPeriod.title = "10MIN BREAK";
+			breakPeriod.sched = "11:50-12:00";
+			updateDisplay(breakPeriod);
+			$('.next-period').html("PSY 101/B-106");
+		}
+		// 12:00-13:19AM
+		else if (hr == 12 || (hr == 13 && min < 20)) {
+			updateDisplay(psy);
+			window.setTimeout(function() {
 				$('.container').height('424px');
 				$('footer').hide();
-				$('.circle-mask').css('border', '15px solid white');
-				$('.clock-wipe-mask').css('border', '2px solid #FFAA30');
-				$('.clock-wipe-mask, .circle-mask, body').css('background', '#FFAA30');
-			}
+			},4);
 		}
 
-		// Tuesday, Thursday
-		else if (day == 2 || day == 4) {
-			// 12:00-8:29AM
-			if (hr < 8 || (hr == 8 && min < 30)) {
-				breakPeriod.title = "PREP";
-				breakPeriod.time = "0:00-8:30";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("CS 21B/F-227");
-			}
-			// 8:30-10:19AM
-			else if ((hr == 8 && min >= 30) || hr == 9 || (hr == 10 && min < 20)) {
-				updateDisplay(cs);
-				$('.next-period').html("90MIN BREAK");
-			}
-			// 10:20-11:49AM
-			else if ((hr == 10 && min >= 20) || (hr == 11 && min < 50)) {
-				breakPeriod.title = "90MIN BREAK";
-				breakPeriod.time = "10:20-11:50";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("MA 20.2/SEC-A 202A");
-			}
-			// 11:50-11:59AM
-			else if (hr == 11 && (min >= 50 && min <= 59)) {
-				breakPeriod.title = "10MIN BREAK";
-				breakPeriod.time = "11:50-12:00";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("MA 20.2/SEC-A 202A");
-			}
-			// 12:00-1:19PM
-			else if (hr == 12 || (hr == 13 && min < 20)) {
-				ma.venue = "SEC-A 202A";
-				ma.time = "12:00-13:20";
-				updateDisplay(ma);
-				$('.next-period').html("30MIN BREAK");
-			}
-			// 1:20-1:49PM
-			else if (hr == 13 && (min >= 20 && min < 50)) {
-				breakPeriod.title = "30MIN BREAK";
-				breakPeriod.time = "13:20-13:50";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("PE 112/COV COURTS");
-			}
-			// 1:50-1:59PM
-			else if (hr == 13 && (min >= 50 && min <= 59)) {
-				breakPeriod.title = "10MIN BREAK";
-				breakPeriod.time = "13:50-14:00";
-				updateDisplay(breakPeriod);
-				$('.next-period').html("PE 112/COV COURTS");
-			}
-			// 2:00-2:49PM
-			else if (hr == 14 && min < 50) {
-				updateDisplay(pe);
-				window.setTimeout(function() {
-					$('.container').height('424px');
-					$('footer').hide();
-				},4);
-			} else {
-				$('.period').html("FIN");
-				$('.container').height('424px');
-				$('footer').hide();
-				$('.circle-mask').css('border', '15px solid white');
-				$('.clock-wipe-mask').css('border', '2px solid #FFAA30');
-				$('.clock-wipe-mask, .circle-mask, body').css('background', '#FFAA30');
-			}
+		else {
+			$('.period').html("FIN");
+			$('.container').height('424px');
+			$('footer').hide();
+			$('.circle-mask').css('border', '15px solid white');
+			$('.clock-wipe-mask').css('border', '2px solid #FFAA30');
+			$('.clock-wipe-mask, .circle-mask, body').css('background', '#FFAA30');
 		}
 
 		$('.clock-wipe-mask').css({
@@ -323,7 +201,7 @@ $(document).ready(function() {
 	});
 
 	var i = 0;
-	var periodTextTimeout;
+	var periodInfoTimeout;
 	$('.circle-mask').click(function() {
 		if ($('.period').html() != "FIN") {
 			if (i < 2) {
@@ -342,12 +220,12 @@ $(document).ready(function() {
 				});
 			} else if (i == 1) {
 				$('.period').css('opacity', '0').on('transitionend', function() {
-					$('.period').html(currentPeriod.venue).css('opacity', '1');
+					$('.period').html(currentPeriod.room).css('opacity', '1');
 				});
 			} else {
 				$('.period').css('opacity', '0').on('transitionend', function() {
-					var periodStart = currentPeriod.time.split("-")[0];
-					var periodEnd = currentPeriod.time.split("-")[1];
+					var periodStart = currentPeriod.sched.split("-")[0];
+					var periodEnd = currentPeriod.sched.split("-")[1];
 
 					var hrStart = periodStart.split(":")[0];
 					var minStart = periodStart.split(":")[1];
@@ -367,8 +245,8 @@ $(document).ready(function() {
 				});
 			}
 
-			clearTimeout(periodTextTimeout);
-			periodTextTimeout = window.setTimeout(function() {
+			clearTimeout(periodInfoTimeout);
+			periodInfoTimeout = window.setTimeout(function() {
 				if (i != 0) {
 					i = 0;
 					$('.period').css('opacity', '0').on('transitionend', function() {
